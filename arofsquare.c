a0310372bd3ab929b5219635f64cc26607890c3d
cprincej <princechaurashiya163@gmail.com>
2023-02-16 23:08:36 +0530

    area of square

---
+++ arofsquare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("inter a\n");
+    scanf("%d", & a);
+    int b;
+    printf("inter b\n");
+    scanf("%d",& b);
+    printf("area of square :%d\n",a*b);
+    return 0;
+}
