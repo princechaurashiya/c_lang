ba4d8399b8197daa797e486d80ac6a7cdaa0af40
cprincej <princechaurashiya163@gmail.com>
2023-02-18 14:47:11 +0530

    addition of two number

---
+++ add.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int a,b,c;
+   printf("do number bhej do");
+   scanf("%d %d",& a,&b);
+   c = a + b;
+   printf("The sum of %d and %d is %d \n",a,b,c);
+}
---
+++ pp.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main(){
+    unsigned short int a = -2;
+    printf("%d \n",a);
+    
+}
