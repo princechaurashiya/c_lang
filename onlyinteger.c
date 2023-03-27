15da8f3cc688b24e84c2b6351871c3bcbf9ab385
cprincej <princechaurasiya163@gmail.com>
2023-03-27 20:58:08 +0530

    wrire a c program to allow the user to enter integer number use untill the user Enter negative number or zero.

---
+++ onlyinteger.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main(){
+    int a ;
+        printf("Enter number\n");
+        scanf("%d",&a);
+        while (a !=0)
+        {
+            if (a<0)
+            break;
+            printf("Enter number\n");
+            scanf("%d",&a);
+        }
+        printf("your last number is not a natural numbr.\n");
+    }
