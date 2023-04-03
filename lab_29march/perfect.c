c57c0b66cbd552dd5520428b7eb23758463eb51d
cprincej <princechaurashiya163@gmail.com>
2023-04-03 14:36:21 +0530

    Checking the given number is a perfec no. or not

---
+++ lab_29march/perfect.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int perfect (int);
+void main(){
+    int a,b,c;
+    printf("Enter number:");
+    scanf("%d",&a);
+     c = perfect(a);
+    if (c == a)
+    {
+        printf("Given number %d is perfect number\n",a);
+    }
+    else {
+    printf("given number %d is not perfect number\n",a);
+}
+}
+ int perfect(int x){
+    int sum;
+ for (int i=1; i<x; i++)
+{
+    if (x %i==0)
+     sum=sum+i;
+}
+return sum;
+}
