950aac628015ff2183c6a50b81794e0693c78d40
cprincej <princechaurashiya163@gmail.com>
2023-04-03 23:19:01 +0530

    taking any no.from user and finding  next prime number

---
+++ lab_29march/prime.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int i,n,j;
+  int  count;
+  printf("Enter number:");
+  scanf("%d",&n);
+  if (n==0)
+  n=n+1;
+
+
+    for (i=n+1; i>0; i++)
+    {
+        count=0;
+      for (j=2; j<= sqrt(i); j++)
+      {
+      if (i % j==0){
+        count =1;
+       // break;
+      }
+      }
+      if(count == 0){
+      printf("Next prime number is:%d\n",i);
+      break;
+      }
+    }
+    return 0;
+}
