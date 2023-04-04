ca923514f40b8c2d41b9b29cf976ad3661c735bc
cprincej <princechaurashiya163@gmail.com>
2023-04-04 18:00:05 +0530

    5 Find the differenece between  consecutive element of arry & largest difference

---
+++ lab_29march/arryld.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main (){
+int n,i;
+printf("Enter size of arry:");
+scanf("%d",&n);
+int a[n] ,b[n-1];
+for (i=0;i<n;i++)
+{
+    printf("Enter the value of element of arry:");
+    scanf("%d",&a[i]);
+}
+for (i=0; i<n-1; i++)
+{
+    b[i] =a[i] - a[1+i] ;
+}
+printf("The difference between two consecutive elemetn are:\n");
+for(i=0;i < n-1; i++)
+{
+    printf("%u\n",b[i]);
+}
+int max_diff =b[0];
+for (i=0;i<n-1;i++)
+{
+    if (max_diff < b[i])
+    max_diff = b[i];
+}
+printf("Largest difference between consecutive element is:%d\n",max_diff);
+}
