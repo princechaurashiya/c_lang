b77d7418258609996e2d58f24a4f32b71b1ce667
cprincej <princechaurashiya163@gmail.com>
2023-05-12 12:29:48 +0530

    For finding consecutive difference of element & max diff by using function & pointer

--- lab_10may/q1.c
+++ lab_10may/q1.c
@@ -11,19 +11,19 @@ void main(){
     scanf("%d",&a[i]);
     }
     int c = diff(a,n-1);
-    printf("max diff %d\n",c);
+    printf("\nmax diff is: %d\n",c);
     }    
-        
+
     int diff(int *p,int b)
     {
         int max=0,diff[b];
-                 for (int k=0;k<(b);k++)
+        printf("The diff b/w consecutive element is:\n");
+       for (int k=0;k<(b);k++)
         {   
             diff[k]=p[k]- p[k+1];
             printf("%d ",diff[k]);
             if (max<diff[k])
             max=diff[k];
         }   
-        printf("\n%d  \n",max);
         return max;
     }    
